08f51d5e45ce522f848806220f3f27bb40454323
Charles Allen <ca29754@gmail.com>
2024-02-23 03:26:58 -0600

    Version 1 : Testing | Check.cs / Funcs.cs

--- Check.cs
+++ Check.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading;
 using xNet;
@@ -9,8 +11,30 @@ namespace Fortnite
 {
     internal class Check
     {
+        public static ConcurrentQueue<string> Combos = Tools.Combos;
         public static void Checking()
         {
+            for (; ; )
+            {
+                try
+                {
+                    if (Combos.Any<string>() == false) { Thread.Sleep(-1); }
+
+                    string Account;
+                    string Proxy;
+                    string ProxyType = Tools.ProxyType;
+
+                    Combos.TryDequeue(out Account);
+
+                    var Email = Account.Split(new char[] { ':', ';', '|' })[0];
+                    var Password = Account.Split(new char[] { ':', ';', '|' })[1];
+                    var User = Email.Contains("@") ? Email.Split('@')[0] : Email;
+
+                    HttpRequest httpReq = new HttpRequest();
+
+                    httpReq.SslCertificateValidatorCallback += (obj, cert, ssl, error) => (cert as X509Certificate2).Verify();
+                }
+            }
         }
     }
 }
---
+++ Funcs.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fortnite
+{
+    internal class Funcs
+    {
+        public static string ParseToken(string content, string leftString, string rightString)
+        {
+            var returnThis = content.Split(new string[] { leftString }, StringSplitOptions.None)[1];
+            returnThis = returnThis.Split(new string[] { rightString }, StringSplitOptions.None)[0];
+            return returnThis;
+        }
+
+        /// <summary>
+        /// IGNORE THIS I WILL FIX LATER JUST TEMP FOR NOW ;)
+        /// </summary>
+        public static void CalcCPM()
+        {
+            for (; ; )
+            {
+                Tools.Cpm_Builder = 0;
+                Thread.Sleep(1000);
+                Tools.Cpm_Final = Tools.Cpm_Builder;
+                Tools.Cpm_Final *= 60;
+            }
+        }
+
+        ///
+        // IF YOU WANT TO USE DISCORD RPC HERE I MADE THIS. YOU CODE THE REST
+        ///
+        /*public static void RPCUpdater()
+        {
+            Thread.Sleep(500);
+
+            for (; ; )
+            {
+                string Status = Tools.RunningThreads > 0 ? "Running" : "Paused";
+                string Details = string.Format("Hits: {0} - Free: {1} - Bads: {2} - CPM: {3} - {4}", new object[]
+                {
+                    Tools.Hits,
+                    Tools.Frees,
+                    Tools.Bads,
+                    Tools.Cpm_Final,
+                    Status,
+                });
+
+                string status = $"Checking: {Tool.Toolname}";
+                RPC.Handler.UpdatePresence(Details, status);
+                Thread.Sleep(5000);
+            }
+        }*/
+    }
+}
